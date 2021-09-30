64c3dd159247cbf63203e31899b6a19f5c8874bc
xllwhoami <xllwhoami@gmail.com>
2021-09-30 19:53:20 +0300

    added function memory_to_string

---
+++ cwutils/__init__.py
@@ -0,0 +1,35 @@
+import re
+
+def memory_to_string(input):
+	parsed = re.findall(r'([0-9]+)\s+to\s+(KB|MB|GB|TB|PB|EB|ZB|YB)', input)
+	
+	if not parsed[0]:
+		return
+	
+	bytes = float(parsed[0][0])
+	memory_type = parsed[0][1]
+	unit_size = 1000
+	
+	if memory_type == 'KB':
+		result = unit_size
+	if memory_type == 'MB':
+		result = unit_size**2
+	if memory_type == 'GB':
+		result = unit_size**3
+	if memory_type == 'TB':
+		result = unit_size**4
+	if memory_type == 'PB':
+		result = unit_size**5
+	if memory_type == 'EB':
+		result = unit_size**6
+	if memory_type == 'ZB':
+		result = unit_size**7
+	if memory_type == 'YB':
+		result = unit_size**8
+	
+	return bytes/result
+
+	
+	
+	
+print(memory_to_string('1000000 to KB'))
