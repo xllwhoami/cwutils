7e9bc8d1f87c839207bf1c10ac5602da12b530d5
xllwhoami <xllwhoami@gmail.com>
2021-09-30 18:38:55 +0300

    0.0.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='cwutils',
+	version='1.0.0',
+	description='... simple utils',
+	author='Chaos and whoami.exe',
+	author_email='nonono@nono.no',
+	url='web.nono.no',
+	packages=['cwutils']
+)
