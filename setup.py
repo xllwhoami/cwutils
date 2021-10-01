6d7906dbb1e47a8dfddf9f1609df86b0cbe8f33f
xllwhoami <xllwhoami@gmail.com>
2021-10-01 15:08:04 +0300

    added more memory funcs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='cwutils',
-	version='1.0.0',
+	version='1.1.0',
 	description='... simple utils',
 	author='Chaos and whoami.exe',
 	author_email='nonono@nono.no',
