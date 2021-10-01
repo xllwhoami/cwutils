06414d96afefb3c5cf2581227f6329e61d56b3c6
xllwhoami <xllwhoami@gmail.com>
2021-10-01 21:30:47 +0300

    bugs fixed

---
+++ cwutils/time.py
@@ -0,0 +1,4 @@
+from humanfriendly import format_timespan
+
+def time_to_string(utime):
+	return format_timespan(utime)
--- x.py
+++
@@ -1,6 +0,0 @@
-from cwutils import memory
-
-res = memory.memory_from_string('1 MB')
-print(
-memory.memory_to_string(1000000
-))
