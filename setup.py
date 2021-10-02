2106d9c5ec2a1536692820231d273d3761246134
xllwhoami <86256058+xllwhoami@users.noreply.github.com>
2021-10-02 19:14:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
-
-with open('README.md') as f:
-       long_description = f.read()
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory/"README.md").read_text()
 
