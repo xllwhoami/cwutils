d953fd40d450eca2528642beda12e3d5b0ccb4b3
xllwhoami <86256058+xllwhoami@users.noreply.github.com>
2021-10-02 18:30:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-with open('README.md') as f:
-        long_description = f.read()
+#with open('README.md') as f:
+       # long_description = f.read()
 
 setup(
 	name='cwutils',
 	version='1.1.0',
 	description='test',
-    long_description=long_description,
+    #long_description=long_description,
 	author='Chaos and whoami.exe',
 	author_email='nonono@nono.no',
 	url='https://xllmoon.web.app',
