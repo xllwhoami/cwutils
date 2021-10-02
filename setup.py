0d14eb2e0d7a7e332997534ae9128118de641ed6
xllwhoami <xllwhoami@gmail.com>
2021-10-02 15:20:48 +0300

    mini fixes

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
 	name='cwutils',
 	version='1.1.0',
 	description='test',
-        long_description=long_description,
+    long_description=long_description,
 	author='Chaos and whoami.exe',
 	author_email='nonono@nono.no',
 	url='https://xllmoon.web.app',
 	install_requires = [
 		'humanfriendly',
-                'num2words'
+        'num2words'
 	],
 	packages=['cwutils']
 )
