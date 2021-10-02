91789584b0eb9355d151e0f9d99934efffd0bf0d
xllwhoami <86256058+xllwhoami@users.noreply.github.com>
2021-10-02 15:17:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
 
+with open('README.md') as f:
+        long_descripotion = f.read()
+
 setup(
 	name='cwutils',
 	version='1.1.0',
 	description='test',
+        long_description=long_description,
 	author='Chaos and whoami.exe',
 	author_email='nonono@nono.no',
-	url='https://web.kgbot.pp.ua',
+	url='https://xllmoon.web.app',
 	install_requires = [
-		'humanfriendly'
+		'humanfriendly',
+                'num2words'
 	],
 	packages=['cwutils']
 )
